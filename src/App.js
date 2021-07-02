5af6fc047309403929aa3a1af7d8de25560cac94
ambikabongirwar@gmail.com <ambikabongirwar@gmail.com>
2021-07-02 18:45:25 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path = "/checkout/:someRandomId/page">
+            <h1>Checkout</h1>
+          </Route>
+          <Router path = "/login">
+            <h1>Login page</h1>
+          </Router>
+          <Route path = "/">
+            <h1>Home Page!!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
